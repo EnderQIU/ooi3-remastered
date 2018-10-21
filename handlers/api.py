ff9696f07044ec1794ea6fc41ce68959a451e20e
enderqiu <a934560824@163.com>
2018-10-21 23:21:52 +0800

    Use requests instead of aiohttp in api.py

--- handlers/api.py
+++ handlers/api.py
@@ -1,13 +1,13 @@
 """转发客户端FLASH和游戏服务器之间的通信。
 接受客户端FLASH发送到OOI3服务器的请求，将其转发给用户所在的游戏服务器，获得响应后再返回给客户端FLASH。
 """
-import os
 
+import requests
 import aiohttp
 import aiohttp.web
-import asyncio
 from aiohttp_session import get_session
-from yarl import URL
+
+from base import config
 
 
 class APIHandler:
@@ -18,19 +18,14 @@ class APIHandler:
 
         :return: none
         """
-        proxy = os.environ.get('HTTP_PROXY')
-        if proxy is None:
-            proxy = os.environ.get('HTTPS_PROXY')
-        if proxy:
-            self.proxy = proxy
-        else:
-            self.proxy = None
+        self.session = requests.Session()
+        self.session.proxies = config.proxies
+        self.session.timeout = 5
 
         # 初始化存放镇守府图片和api_start2内容的变量
         self.api_start2 = None
         self.worlds = {}
 
-    @asyncio.coroutine
     def world_image(self, request):
         """ 显示正确的镇守府图片。
         舰娘游戏中客户端FLASH请求的镇守府图片是根据FLASH本身的URL生成的，需要根据用户所在的镇守府IP为其显示正确的图片。
@@ -47,19 +42,17 @@ class APIHandler:
             if image_name in self.worlds:
                 body = self.worlds[image_name]
             else:
-                url = URL('http://203.104.209.102/kcs/resources/image/world/' + image_name + '.png')
-                coro = aiohttp.ClientRequest(url=url, method='GET', proxy=self.proxy)
+                url = 'http://203.104.209.102/kcs/resources/image/world/' + image_name + '.png'
                 try:
-                    response = yield from asyncio.wait_for(coro, timeout=5)
-                except asyncio.TimeoutError:
+                    response = self.session.get(url=url)
+                except requests.exceptions.Timeout:
                     return aiohttp.web.HTTPBadRequest()
-                body = yield from response.read()
+                body = response.content
                 self.worlds[image_name] = body
             return aiohttp.web.Response(body=body, headers={'Content-Type': 'image/png', 'Cache-Control': 'no-cache'})
         else:
             return aiohttp.web.HTTPBadRequest()
 
-    @asyncio.coroutine
     def api(self, request):
         """ 转发客户端和游戏服务器之间的API通信。
 
@@ -77,20 +70,19 @@ class APIHandler:
                 referrer = request.headers.get('REFERER')
                 referrer = referrer.replace(request.host, world_ip)
                 referrer = referrer.replace('https://', 'http://')
-                url = URL('http://' + world_ip + '/kcsapi/' + action)
+                url = 'http://' + world_ip + '/kcsapi/' + action
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
                     'Origin': 'http://' + world_ip + '/',
                     'Referer': referrer,
                 }
-                data = yield from request.post()
-                coro = aiohttp.ClientRequest(url=url, method='POST', data=data, headers=headers, proxy=self.proxy)
+                data = request.post()
                 try:
-                    response = yield from asyncio.wait_for(coro, timeout=5)
-                except asyncio.TimeoutError:
+                    response = self.session.post(url=url, data=data, headers=headers)
+                except requests.exceptions.Timeout:
                     return aiohttp.web.HTTPBadRequest()
-                body = yield from response.read()
-                if action == 'api_start2' and len(body) > 100000:
+                body = response.content
+                if action == 'api_start2' and len(response.content) > 100000:
                     self.api_start2 = body
                 return aiohttp.web.Response(body=body, headers={'Content-Type': 'text/plain'})
         else:

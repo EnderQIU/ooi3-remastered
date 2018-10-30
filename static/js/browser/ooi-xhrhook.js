3bc00c085fc54fe863d5adeb319031f1d607216e
enderqiu <a934560824@163.com>
2018-10-30 11:13:24 +0800

    fix trigger execute timing

--- static/js/browser/ooi-xhrhook.js
+++ static/js/browser/ooi-xhrhook.js
@@ -3,9 +3,9 @@
  * Principle:
  * main.js?version=4.2.1.0:formatted @line: 334
  * The KCS uses the axios js which is based on XMLHttpRequest but it won't
- * work if you just hook the xhr in the outer page. That because the request
- * was created from the index.php iframe which inside the top page. So you
- * must hook xhr from the iframe instead of root page.
+ * work if you just hook the xhr out of the game iframe. That because the request
+ * was created from the index.php iframe which is inside the top page. So you
+ * must hook xhr from "window.iframs[0]" instead of the root page.
  *
  * Hook Example:
  *
@@ -34,19 +34,20 @@
  * }
  *
  * Usage:
- * $.ooiXhrEventEmitter.on('beforeXhrSend', function(config){...});
- * $.ooiXhrEventEmitter.on('onXhrReceive', function(response){...});
+ *
+ * $("#ooi-trigger").on("ooiXhrEventEmitterReady", function () {
+ *     let trigger = $("#ooi-trigger");
+ *     trigger.on('beforeXhrSend', function(event, config){console.log(config)});
+ *     trigger.on('onXhrReceive', function(event, response){console.log(response)});
+ * });
 */
 
 document.getElementById('htmlWrap').onload = function () {
-    let term = new Terminal();
-    term.open(document.getElementById('debugTerminal'));
-    $.ooiXhrEventEmitter = $({});
 
     // Add a request interceptor
     window.frames[0].axios.interceptors.request.use(function (config) {
         // Do something before request is sent
-        $.ooiXhrEventEmitter.trigger('beforeXhrSend', config);
+        $("#ooi-trigger").trigger('beforeXhrSend', config);
         return config;
     }, function (error) {
         // Do something with request error
@@ -56,10 +57,14 @@ document.getElementById('htmlWrap').onload = function () {
     // Add a response interceptor
     window.frames[0].axios.interceptors.response.use(function (response) {
         // Do something with response data
-        $.ooiXhrEventEmitter.trigger('onXhrReceive', response);
+        $("#ooi-trigger").trigger('onXhrReceive', response);
         return response;
     }, function (error) {
         // Do something with response error
         return Promise.reject(error);
     });
+
+    // OOI XHR event emitter ready state trigger,
+    // all event above should be triggered after the emitter is ready.
+    $("#ooi-trigger").trigger('ooiXhrEventEmitterReady');
 };
--- util/assets.py
+++ util/assets.py
@@ -7,41 +7,41 @@ from flask_assets import Bundle
 bundles = {
     # base.html
     'base_js': Bundle(
-        'js/ooi-base.js',
+        'js/base/ooi-base.js',
         output='dist/base.js',
         filters='jsmin',
     ),
     'base_css': Bundle(
-        'css/ooi-base.css',
+        'css/base/ooi-base.css',
         output='dist/base.css',
         filters='cssmin',
     ),
     # form.html
     'form_js': Bundle(
-        'js/ooi-tweets.js',
+        'js/form/ooi-tweets.js',
         output='dist/form.js',
         filters='jsmin',
     ),
     # browser.html
     'browser_js': Bundle(
-        'js/ooi-xhrhook.js',
-        'js/ooi-fullscreen.js',
+        'js/browser/ooi-xhrhook.js',
+        'js/browser/ooi-fullscreen.js',
         output='dist/browser.js',
         filters='jsmin',
     ),
     'browser_css': Bundle(
-        'css/ooi-browser.css',
+        'css/browser/ooi-browser.css',
         output='dist/browser.css',
         filters='cssmin',
     ),
     # poi.html
     'poi_js': Bundle(
-        'js/ooi-poi.js',
+        'js/poi/ooi-poi.js',
         output='dist/poi.js',
         filters='jsmin',
     ),
     'poi_css': Bundle(
-        'css/ooi-poi.css',
+        'css/poi/ooi-poi.css',
         output='dist/poi.css',
         filters='cssmin',
     ),

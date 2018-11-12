c836330a34ad2204514f4a215b6d9be2c215b43a
enderqiu <a934560824@163.com>
2018-11-12 17:43:28 +0800

    ios docking accomplish notification

--- static/js/ios/ooi-iosjsbridge.js
+++ static/js/ios/ooi-iosjsbridge.js
@@ -1,20 +1,55 @@
+function resp2Json(resp) {
+    return JSON.parse(resp.data.replace('svdata=', ''));
+}
+
+function getCompleteIntervalFromData(data, ahead) {
+    if (ahead)
+        return Math.floor((new Date(data.api_data.api_complatetime) - new Date()) / 1000) - 60;
+    else
+        return Math.floor((new Date(data.api_data.api_complatetime) - new Date()) / 1000);
+}
+
+function getCompleteIntervalFromAPIData(api_data, ahead) {
+    if (ahead)
+        return Math.floor((new Date(api_data.api_completetime) - new Date()) / 1000) - 60;
+    else
+        return Math.floor((new Date(api_data.api_completetime) - new Date()) / 1000);
+}
+
 function sendXhrResponseToSwift(resp) {
     try {
         if (resp.request.responseURL.indexOf("/kcsapi/api_req_mission/start") !== -1) {
-            let data = resp.data;
-            data = JSON.parse(data.replace('svdata=', ''));
-            let complete_time = new Date(data.api_data.api_complatetime);
-            let interval = Math.floor((complete_time - new Date()) / 1000) - 60;  // notify advance one minute
-            SetIntervalNotification(interval, "Expedition accomplished!", "遠征から戻って来ました");
+            // Expedition accomplishment notification
+            let data = resp2Json(resp);
+            let interval = getCompleteIntervalFromData(data, true);
+            SetIntervalNotification("expedition" + data.api_data.api_complatetime_str,
+                interval,
+                "Expedition accomplished!",
+                "遠征から戻って来ました");
+        }
+        else if (resp.request.responseURL.indexOf("/kcsapi/api_get_member/ndock") !== -1) {
+            // Docking accomplishment notification
+            let data = resp2Json(resp);
+            for (let api_data of data.api_data) {
+                if (api_data.api_state > 0) {
+                    // -1: locked; 0: empty; 1: in use
+                    let interval = getCompleteIntervalFromAPIData(api_data, true);
+                    SetIntervalNotification("docking" + api_data.api_complete_time_str,
+                        interval,
+                        "Docking completed!",
+                        "修理、完了なんです");
+                }
+            }
         }
     } catch (e) {
         console.log('Exception occurred when handling XHR response.')
     }
 }
 
-function SetIntervalNotification(interval, title, body) {
+function SetIntervalNotification(identifier, interval, title, body) {
     try {
         webkit.messageHandlers.timeIntervalNotificationTriggerHandler.postMessage({
+            identifier: identifier,
             interval: interval,
             title: title,
             body: body

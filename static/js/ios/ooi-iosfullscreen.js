ec7f99535dcf5a420e1098051993de78fd02b2e6
enderqiu <a934560824@163.com>
2018-11-15 19:54:44 +0800

    Fit devices whose aspect ratio less than 1.67 (etc. iPad)

--- static/js/ios/ooi-iosfullscreen.js
+++ static/js/ios/ooi-iosfullscreen.js
@@ -1,9 +1,18 @@
 function setScale() {
-    let scale = window.screen.width / 720.0;
+    let scale = 1.0;
+    if (document.body.scrollWidth / document.body.scrollHeight < 1.67){
+        // iPad
+        scale = window.screen.height / 1200.0;
+    }else{
+        // iPhone
+        scale = window.screen.width / 720.0;
+    }
     let viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute('content', 'width=device-width user-scalable=yes initial-scale=' + scale.toFixed(2));
 
     // center the iframe
     let left = (document.body.scrollWidth  - 1200) / 2;
+    let top = (document.body.scrollHeight  - 720) / 2;
     document.getElementById('htmlWrap').style.marginLeft = left + 'px';
+    document.getElementById('htmlWrap').style.marginTop = top + 'px';
 }

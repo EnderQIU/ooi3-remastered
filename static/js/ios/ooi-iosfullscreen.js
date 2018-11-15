76c47a8bd5ea8c85ced510694d374edbbf6db85d
enderqiu <a934560824@163.com>
2018-11-15 19:33:38 +0800

    centered the iframe

--- static/js/ios/ooi-iosfullscreen.js
+++ static/js/ios/ooi-iosfullscreen.js
@@ -1,9 +1,9 @@
 function setScale() {
-    // center the iframe
-    let left = ($(window).width() - 1200) / 2;
-    $("#htmlWrap").style.marginLeft = left + 'px';
-
     let scale = window.screen.width / 720.0;
     let viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute('content', 'width=device-width user-scalable=yes initial-scale=' + scale.toFixed(2));
+
+    // center the iframe
+    let left = ($(window).width() - 1200) / 2;
+    $("#htmlWrap").style.marginLeft = left + 'px';
 }
